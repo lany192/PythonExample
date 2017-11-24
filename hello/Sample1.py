a91fbe36ee589c206591d7978dcaf15112dce9d0
Lany <lany168@qq.com>
2017-11-24 17:46:27 +0800

    update

--- hello/Sample1.py
+++ hello/Sample1.py
@@ -11,9 +11,12 @@
 import numpy as np  # 这是Python的一种开源的数值计算扩展，非常强大
 import tensorflow as tf  # 导入tensorflow
 
+A = 0.1
+B = 0.8
+
 ##构造数据##
 x_data = np.random.rand(100).astype(np.float32)  # 随机生成100个类型为float32的值
-y_data = x_data * 0.1 + 0.3  # 定义方程式y=x_data*A+B
+y_data = x_data * A + B  # 定义方程式y=x_data*A+B
 ##-------##
 
 ##建立TensorFlow神经计算结构##
@@ -31,7 +34,7 @@ init = tf.initialize_all_variables()  # 初始化TensorFlow训练结构
 sess = tf.Session()  # 建立TensorFlow训练会话
 sess.run(init)  # 将训练结构装载到会话中
 
-for step in range(400):  # 循环训练400次
+for step in range(2000):  # 循环训练2000次
     sess.run(train)  # 使用训练器根据训练结构进行训练
     if step % 20 == 0:  # 每20次打印一次训练结果
         print(step, sess.run(weight), sess.run(biases))  # 训练次数，A值，B值
