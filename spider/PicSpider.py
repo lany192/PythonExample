f812d9e18a1254322de8fbfea452d1f9b12ea642
lyg <lany192@qq.com>
2019-04-10 16:32:25 +0800

    修改图片爬虫

--- spider/PicSpider.py
+++ spider/PicSpider.py
@@ -1,19 +1,35 @@
 import re
+import time
+from urllib.error import ContentTooShortError
+from urllib.request import urlretrieve
 
 import requests
 
 
+# 下载保存图片
+def auto_down_pic(url, filename):
+    try:
+        urlretrieve(url, filename)
+    except ContentTooShortError:
+        print("Network conditions is not good.Reloading.")
+        auto_down_pic(url, filename)
+
+
+# 查找照片并下载
 def get_pic_url(url):
-    res = requests.get(url)
-    res.encoding = "utf-8"
-    html = res.text
-    pic_url = re.findall(r'https*://[^\s]*?\.jpg', html, re.S)
-    print(pic_url)
-    # for key in pic_url:
-    #     print(key + "\r\n")
+    html = requests.get(url).text
+    pic_list = re.findall(r'https*://[^\s]*?\.jpg', html, re.S)
+    for key in pic_list:
+        print(key + "\r\n")
+        # 保存图片,注意images要提前创建
+        pic_dir = "./images/"
+        pic_name = pic_dir + str(int(time.time())) + ".jpg"
+        auto_down_pic(key, pic_name)
 
 
+# 第一页开始
 for i in range(4860):
     print("开始爬", i, "页")
     url = "https://www.dbmeinv.com/?pager_offset=" + str(i)
     get_pic_url(url)
+    time.sleep(3)  # 休眠3秒
