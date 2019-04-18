2cc099019fee71854d7adda991cbb94540ca0663
lyg <lany192@qq.com>
2019-04-18 14:14:33 +0800

    update

---
+++ matplotlib/README.md
@@ -0,0 +1,4 @@
+
+Python数据可视化matplotlib.pyplot:
+
+    https://www.jianshu.com/p/85a01b7d6507
--- matplotlib/demo7.py
+++ matplotlib/demo7.py
@@ -42,10 +42,25 @@ class RandomWalk(object):
         pass
 
 
-# 创建一个RandomWalk实例，并将其包含的点都绘制出来
-rw = RandomWalk()
+while True:
+    # 创建一个RandomWalk实例，并将其包含的点都绘制出来
+    rw = RandomWalk()
+    rw.fill_walk()
 
-rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
 
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show()
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+
+    # 突出起点和终点
+    plt.scatter(0, 0, c='green', edgecolor='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # 隐藏坐标轴
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+    plt.show()
+
+    keep_running = input("Make another walk?(y/n)")
+    if keep_running == 'n':
+        break
